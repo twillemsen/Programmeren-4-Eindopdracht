de3d6671144bd84d6fa0aa4e90817222406ae8fb
Stephan <swit2@avans.nl>
2018-05-14 13:59:00 +0200

    Added first POST-feature

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 // const routes
+const studentenhuis_routes = require('./routes/studentenhuis_routes');
 // const ApiError
+const ApiError = require('./model/ApiError');
 const config = require('./config/config');
+
 
 const port = process.env.PORT || config.webPort;
 
@@ -13,6 +16,7 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
+app.use('/api', studentenhuis_routes);
 
 // Test endpoint:
 app.get('/test', (req, res, next) => {
@@ -20,6 +24,21 @@ app.get('/test', (req, res, next) => {
         "Test": "This is a test endpoint"
     });
 });
+
+app.use('*', (req, res, next) => {
+    console.log('Endpoint does not exist');
+    next('Endpoint does not exist');
+})
+
+app.use((err, req, res, next) => {
+    console.log('Catch-all error handler called.');
+    console.log(err.toString());
+
+    const error = new ApiError(err.toString(), 404);
+
+    res.status(404).json(error).end();
+})
+
 
 app.listen(port, () => {
     console.log('Server running on port ' + port);
