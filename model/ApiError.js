70dbe04f184655e76d34faf68fbd02824efc9324
Tobias Willemsen <twillems4@avans.nl>
2018-05-15 10:17:28 +0200

    Fixed merge conflicts.

--- model/ApiError.js
+++ model/ApiError.js
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
 class ApiError {
     constructor(message, code){
         this.message = message;
         this.code = code;
         this.datetime = Date()
     }
-=======
-// module.exports = {}
-
-class ApiError {
-
-    constructor(message, code){
-        this.message = message;
-        this.code = code;
-        this.datetime = Date();
-    }
-
->>>>>>> 23b8c52b8ecbf4012d3d19f2958ddc92533a4a3b
 }
 
 module.exports = ApiError;
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
 // const routes
 const studentenhuis_routes = require('./routes/studentenhuis_routes');
-=======
 const auth_routes = require('./routes/authentication.routes');
 const auth_controller = require('./controllers/authentication.controller');
->>>>>>> 23b8c52b8ecbf4012d3d19f2958ddc92533a4a3b
 // const ApiError
 const ApiError = require('./model/ApiError');
 const config = require('./config/config');
@@ -22,14 +19,11 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
-<<<<<<< HEAD
-app.use('/api', studentenhuis_routes);
-=======
 app.use('/api', auth_routes);
 
 app.all('*', auth_controller.validateToken);
 
->>>>>>> 23b8c52b8ecbf4012d3d19f2958ddc92533a4a3b
+app.use('/api', studentenhuis_routes);
 
 // Test endpoint:
 app.get('/test', (req, res, next) => {
@@ -38,7 +32,6 @@ app.get('/test', (req, res, next) => {
     });
 });
 
-<<<<<<< HEAD
 app.use('*', (req, res, next) => {
     console.log('Endpoint does not exist');
     next('Endpoint does not exist');
@@ -53,14 +46,6 @@ app.use((err, req, res, next) => {
     res.status(404).json(error).end();
 })
 
-=======
-app.use(function (error, req, res, next) {
-	console.error(error.toString());
-	res.status(500).json({
-		message: error
-	}).end();
-});
->>>>>>> 23b8c52b8ecbf4012d3d19f2958ddc92533a4a3b
 
 app.listen(port, () => {
     console.log('Server running on port ' + port);
