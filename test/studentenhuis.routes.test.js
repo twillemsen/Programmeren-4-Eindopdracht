ee813f454c0ddacd6f549bf1dc36e7a2d41dd5ad
Tobias Willemsen <twillems4@avans.nl>
2018-05-16 14:52:24 +0200

    Added first two studentenhuis.routes testcases.

--- test/studentenhuis.routes.test.js
+++ test/studentenhuis.routes.test.js
@@ -1,24 +1,60 @@
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const server = require('../server')
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
 
-chai.should()
-chai.use(chaiHttp)
+chai.should();
+chai.use(chaiHttp);
 
 describe('Studentenhuis API POST', () => {
     it('should throw an error when using invalid JWT token', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
+        const validToken = require('./authentication.routes.test').token;
+
+        chai.request(server)
+            .post('/api/studentenhuis')
+            .set('x-access-token', 'onzinToken')
+            .send({
+                "naam": "ABCD",
+                "adres": "EFG1"
+            })
+            .end((err, res) => {
+                res.should.have.status(401);
+                res.body.should.be.a('object');
+
+                let response = res.body;
+
+                response.should.have.property('message').equals('No token or wrong token provided');
+                response.should.have.property('code').equals(401);
+                response.should.have.property('datetime').equals(res.body.datetime);
+
+                done();
+            });
+    });
 
     it('should return a studentenhuis when posting a valid object', (done) => {
-        //
-        // Hier schrijf je jouw testcase.
-        //
-        done()
-    })
+        const validToken = require('./authentication.routes.test').token;
+
+        chai.request(server)
+            .post('/api/studentenhuis')
+            .set('x-access-token', validToken)
+            .send({
+                "naam": "ABCD",
+                "adres": "EFGHI1"
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+
+                let response = res.body;
+
+                response.should.have.property('ID');
+                response.should.have.property('Naam').equals(res.body.Naam);
+                response.should.have.property('Adres').equals(res.body.Adres);
+                response.should.have.property('Contact').equals(res.body.Contact);
+                response.should.have.property('Email').equals(res.body.Email);
+
+                done();
+            });
+    });
 
     it('should throw an error when naam is missing', (done) => {
         //
